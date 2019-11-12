8e8cef47116395e51b40f3562f8ecd7785fd984c
Adam Wilson <adam@thousandeyes.com>
2019-11-12 14:15:55 -0500

    Assert - Add example of using assert

---
+++ examples/assertCondition.js
@@ -0,0 +1,16 @@
+import { By } from 'selenium-webdriver';
+import { driver } from 'thousandeyes';
+import assert from 'assert';
+
+runScript();
+
+async function runScript() {
+    
+    await driver.get('https://www.x-rates.com/table/?from=USD&amount=1');
+
+    const usdToEuroRateCell = await driver.findElement(By.css(`[href="http://www.x-rates.com/graph/?from=USD&to=EUR"]`));
+    const currentUsdToEuroRate = Number(usdToEuroRateCell.getText());
+
+    const isOneEuroWorthMoreThanTwoUsd = currentUsdToEuroRate < 0.5;
+    assert(isOneEuroWorthMoreThanTwoUsd, 'One Euro is not worth more than 2 USD!');
+}
