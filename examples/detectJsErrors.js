c14382f0a6c567c403e47663a979f2cdd7e1d9e4
Adam Wilson <adam@thousandeyes.com>
2019-11-13 16:40:38 -0500

    Detect JS Errors - Add example for how to detect JS errors

---
+++ examples/detectJsErrors.js
@@ -0,0 +1,19 @@
+import { logging } from 'selenium-webdriver';
+import { driver } from 'thousandeyes';
+import assert from 'assert';
+
+runScript();
+
+async function runScript() {
+
+    // Throw an error on a delay to simulate an uncaught JS error
+    await driver.executeScript(`setTimeout(() => { throw new Error('Simulate Uncaught Error') }, 0)`);
+    const browserLogs = await driver.manage().logs().get('browser');
+
+    
+    console.log('Browser logs:');
+    console.log(JSON.stringify(browserLogs, null, '  '), '\n');
+    
+    const severeErrors = browserLogs.filter(logEntry => logEntry.level === logging.Level.SEVERE);
+    assert(severeErrors.length === 0, 'Errors found during the page load!')
+}
