8b14d7c7823374640a8e23a4e2832590daf14f34
Bostjan Skufca <bskufca@thousandeyes.com>
2020-04-25 23:46:12 +0200

    examples/totp: Remove bogus and fix space formatting (does not look nice on github) + reword some of the comments

--- examples/usingTOTPTwoFactorAuth.js
+++ examples/usingTOTPTwoFactorAuth.js
@@ -4,21 +4,17 @@ import { driver, credentials, authentication } from 'thousandeyes';
 runScript();
 
 async function runScript() {
-    // Calling `credentials.get` will allow you to retrieve sensitive tokens/passwords saved 
-    // in the ThousandEyes credential store. 
-    // When working in the Recorder, make sure you've either imported or added the credential info by hand
-    // When working in the thousandeyes app, make sure the credential is associated with the test in question
+    // Calling `credentials.get` retrieves sensitive tokens/passwords saved
+    // in the ThousandEyes credential store.
+    // When working in the Recorder, make sure you've either imported or added the credential info by hand.
+    // When working in the ThousandEyes app, make sure the credential is associated with the test in question.
     const secretToken = credentials.get('TOTP Secret Token');
-    
-    
-    // Calling `authentication.getTimeBasedOneTimePassword` will all you to generate a 6-digit
+
+    // Calling `authentication.getTimeBasedOneTimePassword` generates a 6-digit
     // temporary authentication code to be used in conjunction with a password on sites that
     // support 2 factor authentication with time-based one-time passwords (TOTPs).
-	var totp = authentication.getTimeBasedOneTimePassword(secretToken);
-    
-    
+    var totp = authentication.getTimeBasedOneTimePassword(secretToken);
+
     await driver.findElement(By.css('.totp_auth_code'))
                 .sendKeys(totp);
-                
-
 }
