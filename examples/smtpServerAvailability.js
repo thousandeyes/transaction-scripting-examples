ad861857494df22a7b8ec856c7a41ceb8df85de5
mjdobson <mdobson@thousandeyes.com>
2020-06-23 17:26:53 -0500

    v2
    updated per Benton's suggestions

--- examples/smtpServerAvailability.js
+++ examples/smtpServerAvailability.js
@@ -17,19 +17,25 @@ async function callServer(host, port) {
     const sock = await net.connect(port, host);
     sock.setEncoding('utf8');
     let response = await sock.read();
+    
+    // Send Hello message to the server
     await sock.writeAll('HELO ' + host + '\r\n');
     response = await sock.read();
-    // Check entire response with option below when using IDE
+    
+    // Check entire response by uncommenting line below - must use IDE
     //console.log (response);
+    
+    // Change 250 to expected response code if different 
     await checkCode(response,250);
+    
+    // Close connection
     await sock.writeAll('QUIT\r\n');
-    response = await sock.read();
     await sock.end()
 }
 
 async function checkCode(response,correctCode){
     response = response.substring(0,3);
     if (response != correctCode) {
-            assert.fail('Wrong SMTP Response code:' + response);
+            assert.fail('Received wrong SMTP response code: ' + response);
     }
 }
