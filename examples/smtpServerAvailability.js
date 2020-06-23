d4cd18ae07f78f78f1d6395bbb790536d03e28c7
mjdobson <mdobson@thousandeyes.com>
2020-06-23 16:31:24 -0500

    SMTP Server Availability Check
    It's a simple script that checks the availability of an SMTP Server by connecting and evaluating the response code.  I improved on some code Bernie Clairmont provided per case S-CS-0164088 such that the script will fail if the response code is not as expected.

---
+++ examples/smtpServerAvailability.js
@@ -0,0 +1,35 @@
+/* 
+    A script that checks availability of an SMTP Server by connecting to the service and evaluating the response code.
+*/
+
+import { By, Key, until } from 'selenium-webdriver';
+import { net, driver, markers, credentials, downloads, transaction, authentication } from 'thousandeyes';
+import assert from 'assert';
+runScript();
+
+async function runScript() {
+    // Set host and port
+    let host = 'smtp.gmail.com';
+    let port = 587;
+    let resp = await callServer(host, port);
+
+};
+async function callServer(host, port) {
+    const sock = await net.connect(port, host);
+    sock.setEncoding('utf8');
+    let response = await sock.read();
+    await sock.writeAll('HELO ' + host + '\r\n');
+    response = await sock.read();
+    // Check entire response with option below when using IDE
+    //console.log (response);
+    await checkCode(response,250);
+    await sock.writeAll('QUIT\r\n');
+    response = await sock.read();
+}
+
+async function checkCode(response,correctCode){
+    response = response.substring(0,3);
+    if (response != correctCode) {
+            assert.fail('Wrong SMTP Response code:' + response);
+    }
+}
