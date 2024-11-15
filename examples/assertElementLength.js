d67e0661264b53959257e022ee8b7ee31fdfd64e
jeremsta-te <jeremsta@thousandeyes.com>
2024-11-15 16:44:09 -0600

    Update assertElementLength.js
    Add wait(until.elementsLocated...

--- examples/assertElementLength.js
+++ examples/assertElementLength.js
@@ -11,6 +11,8 @@ const settings = test.getSettings();
 // Load page
 await driver.get(settings.url);
 
+await driver.wait(until.elementsLocated(By.xpath("//article[contains(@id, 'post')]"));
+     
 let elementList = await driver.findElements(By.xpath("//article[contains(@id, 'post')]"));
      assert(elementList.length > 3, "Not enough elements found: " + elementList.length);
 }
